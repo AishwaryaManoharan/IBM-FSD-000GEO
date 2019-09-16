fe3f10f37708904c56de0a92c68cb19ecf9772f8
Aishwarya M <aishwaryaengr@gmail.com>
2019-09-17 00:05:32 +0530

    Microservices - Practice

---
+++ Eclipse_Workspace/Microservices/user-service/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,78 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.UserDto;
+import com.example.demo.model.CreateUserRequestModel;
+import com.example.demo.model.CreateUserResponseModel;
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserService service;
+	
+	@PostMapping("/user")
+	public ResponseEntity<CreateUserResponseModel> createUser(@RequestBody CreateUserRequestModel userDetail){
+		ModelMapper mapper = new ModelMapper();
+		UserDto dto = mapper.map(userDetail, UserDto.class);
+		UserDto dto1 = service.createUser(dto);
+		CreateUserResponseModel user = mapper.map(dto1, CreateUserResponseModel.class);
+		return ResponseEntity.status(HttpStatus.CREATED).body(user);
+	
+	}
+	
+	@GetMapping("/user")
+	public List<ResponseEntity<CreateUserResponseModel>> getUsers(){
+		List<User> users = service.getAllUsers();
+		List<ResponseEntity<CreateUserResponseModel>> list = new ArrayList<ResponseEntity<CreateUserResponseModel>>();
+		ModelMapper mapper = new ModelMapper();
+		for(User u : users) {
+			CreateUserResponseModel res = mapper.map(u, CreateUserResponseModel.class);
+			list.add(ResponseEntity.status(HttpStatus.CREATED).body(res));
+		}
+		return list;
+	}
+	
+	@GetMapping("/user/{id}")
+	public ResponseEntity<CreateUserResponseModel> findUser(@PathVariable("id") String userID){
+		ModelMapper mapper = new ModelMapper();
+		User user = service.findUser(userID);
+		CreateUserResponseModel model = mapper.map(user, CreateUserResponseModel.class);
+		return ResponseEntity.status(HttpStatus.CREATED).body(model);
+		
+	}
+	
+	@DeleteMapping("/user/{id}")
+	public ResponseEntity<CreateUserResponseModel> deleteUser(@PathVariable("id") String userID){
+		ModelMapper mapper = new ModelMapper();
+		User user = service.deleteUser(userID);
+		CreateUserResponseModel model = mapper.map(user, CreateUserResponseModel.class);
+		return ResponseEntity.status(HttpStatus.GONE).body(model);	
+	} 
+	
+	@PutMapping("/user/{id}")
+	public ResponseEntity<CreateUserResponseModel> updateUser(@RequestBody CreateUserRequestModel userDetail,@PathVariable("id") String userID){
+		ModelMapper mapper = new ModelMapper();
+		UserDto dto = mapper.map(userDetail, UserDto.class);
+		UserDto dto1 = service.updateUser(dto, userID);
+		CreateUserResponseModel user = mapper.map(dto1, CreateUserResponseModel.class);
+		return ResponseEntity.status(HttpStatus.CREATED).body(user);
+	
+	}
+}
---
+++ Eclipse_Workspace/Microservices/user-service/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,70 @@
+package com.example.demo.model;
+
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String lastName;
+	private String firstName;
+	private String email;
+	private String password;
+	private String userId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public User() {
+		super();
+	}
+	public User(String lastName, String firstName, String email, String password) {
+		super();
+		
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.email = email;
+		this.password = password;
+	}
+}
